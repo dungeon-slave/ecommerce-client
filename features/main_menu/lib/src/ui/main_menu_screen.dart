216f0f5c50caa18b2fa3e9b0eb453e721d146c5e
snus <vasilevsky.matvey@gmail.com>
2023-06-20 23:39:11 +0300

    [PT-1] main-page Add main_menu widget Add item widget

--- features/main_menu/lib/src/ui/main_menu_screen.dart
+++ features/main_menu/lib/src/ui/main_menu_screen.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:main_menu/src/ui/dish_element.dart';
 
 class MainMenuScreen extends StatelessWidget {
   const MainMenuScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 0, 31, 36),
+      body: Column(
+        children: [
+          DishElement(name: 'Dish 1'),
+          DishElement(name: 'Dish 2'),
+          DishElement(name: 'Dish 3'),
+          DishElement(name: 'Dish 4'),
+          DishElement(name: 'Dish 5'),
+        ],
+      ),
     );
   }
 }
