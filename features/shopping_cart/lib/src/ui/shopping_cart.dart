bc5862e00a3b2ce0f3aa155232c8ccd9f234d1d0
snus <vasilevsky.matvey@gmail.com>
2023-07-10 14:05:33 +0300

    [PT-2] navigation-bar add plugs for navbar widgets

---
+++ features/shopping_cart/lib/src/ui/shopping_cart.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ShoppingCart extends StatelessWidget {
+  const ShoppingCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Shopping cart, soon...'),
+    );
+  }
+}
