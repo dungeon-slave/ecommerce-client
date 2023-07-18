7aa5e60578fb8c4c723b38ff1c206bd31f2df1dd
dungeon-slave <vasilevsky.matvey@gmail.com>
2023-07-18 23:12:48 +0300

    [PT-3] shopping_cart remove unnecessary try/catch in firebase_provider add equatable

--- data/lib/providers/firebase_provider.dart
+++ data/lib/providers/firebase_provider.dart
@@ -5,30 +5,27 @@ import 'package:data/entities/dish_type_enity/dish_type_entity.dart';
 
 class FirebaseProvider {
   Future<List<DishTypeEntity>> fetchDishes() async {
-    try {
-      List<DishTypeEntity> dishesTypes = <DishTypeEntity>[];
+    List<DishTypeEntity> dishesTypes = <DishTypeEntity>[];
 
-      final QueryDocumentSnapshot<Map<String, dynamic>> dishesTypesDoc =
-          (await FirebaseFirestore.instance
-                  .collection(AppConstants.menuCollection)
-                  .get())
-              .docs.first;
+    final QueryDocumentSnapshot<Map<String, dynamic>> dishesTypesDoc =
+        (await FirebaseFirestore.instance
+                .collection(AppConstants.menuCollection)
+                .get())
+            .docs
+            .first;
 
-      Map<String, dynamic> fbDishesTypes = dishesTypesDoc.data();
+    Map<String, dynamic> fbDishesTypes = dishesTypesDoc.data();
 
-      for (String key in fbDishesTypes.keys) {
-        final List<DishEntity> dishes = <DishEntity>[];
-        for (Map<String, dynamic> dish in fbDishesTypes[key]) {
-          dishes.add(DishEntity.fromJson(dish));
-        }
-        dishesTypes.add(
-          DishTypeEntity(typeName: key, dishesEntities: dishes),
-        );
+    for (String key in fbDishesTypes.keys) {
+      final List<DishEntity> dishes = <DishEntity>[];
+      for (Map<String, dynamic> dish in fbDishesTypes[key]) {
+        dishes.add(DishEntity.fromJson(dish));
       }
-
-      return dishesTypes;
-    } catch (e) {
-      rethrow;
+      dishesTypes.add(
+        DishTypeEntity(typeName: key, dishesEntities: dishes),
+      );
     }
+
+    return dishesTypes;
   }
 }
