ce7cffa08b773f7e735549ee0147e7bc7823c215
dungeon-slave <vasilevsky.matvey@gmail.com>
2023-08-25 00:27:49 +0300

    [PT-8] tests create unit tests

---
+++ tests/unit_tests/bloc_unit_test.dart
@@ -0,0 +1,32 @@
+import 'package:domain/models/order_history/order_model.dart';
+import 'package:domain/repositories/order_history_repository.dart';
+import 'package:domain/usecase/order_history/fetch_order_history.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:order_history/order_history.dart';
+
+void main() {
+  const OrderHistoryState validState = OrderHistoryState(items: <OrderModel>[]);
+  final OrderHistoryBloc bloc = OrderHistoryBloc(
+    fetchOrderHistoryUseCase: FetchOrderHistoryUseCase(
+      orderHistoryRepository: _OrderHistoryRepositoryTest(),
+    ),
+  );
+
+  group(
+    'HomeScreenBloc',
+    () {
+      test(
+        'Check init state results',
+        () {
+          bloc.add(InitEvent());
+          expect(validState.items, bloc.state.items);
+        },
+      );
+    },
+  );
+}
+
+class _OrderHistoryRepositoryTest implements OrderHistoryRepository {
+  @override
+  Future<List<OrderModel>> fetchOrderHistory() => Future(() => <OrderModel>[]);
+}
---
+++ tests/unit_tests/data_unit_test.dart
@@ -0,0 +1,54 @@
+import 'package:data/entities/dish_entity/dish_entity.dart';
+import 'package:data/mappers/dish_mapper.dart';
+import 'package:domain/models/dishes_items/dish_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group(
+    'DishMapper',
+    () {
+      const DishEntity validEntity = DishEntity(
+        imageRef: '1',
+        name: '1',
+        description: '1',
+        price: 1.00,
+        ingredients: <String>[],
+        id: '1',
+      );
+      const DishModel validModel = DishModel(
+        imageRef: '1',
+        name: '1',
+        description: '1',
+        price: 1.00,
+        ingredients: <String>[],
+        id: '1',
+      );
+
+      test(
+        'Check toModel method on valid data',
+        () {
+          final DishModel result = DishMapper.toModel(validEntity);
+          expect(validModel.imageRef, result.imageRef);
+          expect(validModel.name, result.name);
+          expect(validModel.description, result.description);
+          expect(validModel.price, result.price);
+          expect(validModel.ingredients, result.ingredients);
+          expect(validModel.id, result.id);
+        },
+      );
+
+      test(
+        'Check toEntity method on valid data',
+        () {
+          final DishEntity result = DishMapper.toEntity(validModel);
+          expect(validEntity.imageRef, result.imageRef);
+          expect(validEntity.name, result.name);
+          expect(validEntity.description, result.description);
+          expect(validEntity.price, result.price);
+          expect(validEntity.ingredients, result.ingredients);
+          expect(validEntity.id, result.id);
+        },
+      );
+    },
+  );
+}
